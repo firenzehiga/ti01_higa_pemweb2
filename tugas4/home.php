d760d75cd6bceb50213d7559c7b0092890ac1570
firenzehiga <firenzehiga@gmail.com>
2024-03-16 11:28:21 +0700

    tugas 4

---
+++ tugas4/home.php
@@ -0,0 +1,82 @@
+<?php
+  include_once 'layout/header.php';
+  include_once 'layout/sidebar.php';  
+?>
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+            <div class="col-sm-6">
+                <span class="h4">Dashboard Pegawai</span>
+                <ol class="breadcrumb ">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                </ol>
+            <span class="h1">Welcome Higa!</span>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+
+        <!-- /.row -->
+        <!-- Main row -->
+        <div class="row">
+          <!-- Left col -->
+          <section class="col-lg-12 ">
+            <div class="card card-danger card-outline">
+              <div class="card-header">
+                <h5 class="badge badge-danger" style="font-size: large;">Pengumuman</h5>
+                <div class="card-tools m-0">
+                  <span title="3 New Messages" class="badge badge-primary">3</span>
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="card-body">
+                <h6 class="card-title">Informasi Terkini</h6>
+
+              </div>
+            </div>
+          </section>
+          <!-- /.Left col -->
+          <!-- right col (We are only adding the ID to make the widgets sortable)-->
+          <section class="content col-12">
+            <div class="container-fluid">
+              <div class="row">
+                <!-- /.col -->
+                <div class="col-12">
+                  <div class="card card-primary">
+                    <div class="card-body p-0">
+                      <!-- THE CALENDAR -->
+                      <div id="calendar"></div>
+                    </div>
+                    <!-- /.card-body -->
+                  </div>
+                  <!-- /.card -->
+                </div>
+                <!-- /.col -->
+              </div>
+              <!-- /.row -->
+            </div><!-- /.container-fluid -->
+          </section>
+          <!-- right col -->
+        </div>
+        <!-- /.row (main row) -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <?php
+  include_once 'layout/footer.php';
+  ?>
---
+++ tugas4/layout/header.php
@@ -0,0 +1,41 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PEMWEB | Dashboard</title>
+    <link rel="icon" type="image/x-icon" href="assets/dist/img/nf.ico">
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="assets/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">  
+    <link rel="stylesheet" href="assets/plugins/fullcalendar/main.css">
+    <link rel="stylesheet" href="assets/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="assets/plugins/daterangepicker/daterangepicker.css">
+    </head>
+    <body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+
+    <!-- Preloader -->
+
+
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+        <!-- Left navbar links -->
+        <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+        </ul>
+
+        <!-- Right navbar links -->
+        <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+            <i class="fas fa-expand-arrows-alt"></i>
+            </a>
+        </li>
+        </ul>
+    </nav>
+
